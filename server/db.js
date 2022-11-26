e46b84a37a8fe0911ff99f498278e4529a3f2109
ilchovski98 <ilchovski98@gmail.com>
2022-11-26 18:05:45 +0200

    Production: log data

--- server/db.js
+++ server/db.js
@@ -1,5 +1,11 @@
 const Pool = require('pg').Pool;
-
+console.log('Logging data:', {
+  user: process.env.DB_USER,
+  password: `${process.env.DB_PASSWORD}`,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME
+});
 const pool = new Pool({
   user: process.env.DB_USER,
   password: `${process.env.DB_PASSWORD}`,
